cd57ca6dc60baa6d820899260a90cc7a52497e39
bahar <bahar.mahmoudi5@etu.univ-lorraine.fr>
2026-07-25 15:35:52 +0200

    keeping books based on their popularity and drop books with less than a certain nb of ratings

--- src/goodreads_books_preprocessing.py
+++ src/goodreads_books_preprocessing.py
@@ -9,8 +9,11 @@ import pandas as pd
 from collections import Counter
 import Globals
 
-MIN_SAMPLES = 90
-MAX_PER_CLASS = 700
+MIN_SAMPLES = 100 #90
+MAX_PER_CLASS = 800 #700
+
+TEXT_FEATURES = ['title', 'authors', 'genre', 'description', 'language_code']
+NUM_FEATURES= ['ratings_count', 'text_reviews_count','num_pages', 'publication_year', 'average_rating']
 
 def extract_genre(shelves):
     for shelf in shelves:  # already sorted by count descending
@@ -21,7 +24,7 @@ def extract_genre(shelves):
             return Globals.GENRE_MAPPING[name]
     return None
 
-def load_data(file_name, head=7000):
+def load_data(file_name, head=50000):
     count = 0
     data = []
     with gzip.open(file_name) as fin:
@@ -34,40 +37,59 @@ def load_data(file_name, head=7000):
             if (head is not None) and (count > head):
                 break
     return data
-DIR = '/Users/bahar/Documents/Projects/Books_geek/resources'
 
+def clean_data(df):
+    """This function drops each book with no genre, description, rating bellow 350
+    and gives a popularity score to each book
+    Note: needs to be called after assigning a genre column to the dataframe"""
+    # drop empty descriptions and books with no genre and keep books with 350 < ratings_count
+    df = df[df['description'] != '']
+    df = df[df['genre'].notna()]
+    df = df.dropna(subset=["ratings_count", "average_rating"]) #drop books with none of these features
+    #Convert to numeric
+    df["ratings_count"] = pd.to_numeric(df["ratings_count"], errors="coerce")
+    df["average_rating"] = pd.to_numeric(df["average_rating"], errors="coerce")
+    df = df[df["ratings_count"] >= 200] #drop books with less than 200 ratings
+
+    #assign a score to each book based on the number of rating and avg_rating
+    df["popularity_score"] = (
+            df["average_rating"] * np.log1p(df["ratings_count"])
+    )
+    df = df.sort_values("popularity_score", ascending=False)
+    return df
+
+DIR = '/Users/bahar/Documents/Projects/Books_geek/resources'
 books = load_data(os.path.join(DIR, 'goodreads_books.json.gz'))
 
 #make a dataframe
 df_books = pd.DataFrame(books)
 
-text_features = ['title', 'authors', 'genre', 'description', 'language_code']
-num_features = ['ratings_count', 'text_reviews_count','num_pages', 'publication_year', 'average_rating']
-
 #some info
 #print(df_books.columns.tolist())
-print(df_books.shape)
+#print(df_books.shape)
 #print(df_books.head())
 #print(books[0])
 
 #list of all features for a book
 books_features = df_books.columns.tolist()
+print("All features: ",books_features)
 
 #nulls
 #print(df_books.isnull().sum()) no null
 #print((df_books['description'] == '').sum())
+print(max(df_books["ratings_count"]))
 
 text_cols = ['description', 'title', 'authors']
+
 '''for col in text_cols:
     print(f"{col}: {(df_books[col] == '').sum()} empty")'''
 
 """description: 104 empty -> might need to drop"""
 
-print(df_books['popular_shelves'][0])
 
 #add a top shelf as main category based on more popular shelf
 all_shelves = Counter()
-for shelves in df_books['popular_shelves'][:100]:
+for shelves in df_books['popular_shelves'][:100]: #TODO why the first 100 books???
     for shelf in shelves:
         all_shelves[shelf['name'].lower()] += int(shelf['count'])
 
@@ -76,23 +98,17 @@ for shelves in df_books['popular_shelves'][:100]:
 '''for shelf, count in all_shelves.most_common(40):
     print(f"{shelf}: {count}")'''
 
-#extract genres from common shelves
+#extract genres from common shelves and assign the new column
 df_books['genre'] = df_books['popular_shelves'].apply(extract_genre)
 
 print(df_books['genre'].value_counts())
-#print(df_books['genre'].unique())
+print(df_books['genre'].unique())
 print(df_books['genre'].isnull().sum(), 'books with no matching genre')
-
-# drop empty descriptions and books with no genre
-df_books = df_books[df_books['description'] != '']
-df_books = df_books[df_books['genre'].notna()]
 print(df_books.shape)
 
-#replace bad values with median
-for col in num_features:
-    df_books[col] = pd.to_numeric(df_books[col], errors='coerce')  # converts bad values to NaN
-    df_books[col] = df_books[col].fillna(df_books[col].median()) #replace it with the median
-
+# drop empty descriptions and books with no genre and keep books with 350 < ratings_count
+df_books = clean_data(df_books)
+print(df_books.shape)
 
 # drop small classes first
 valid_genres = df_books['genre'].value_counts()
@@ -112,8 +128,7 @@ print(df_books['genre'].value_counts())
 #print(df_books['authors']) #Only has author's ID
 
 # save cleaned data as a parquet
-df_books.to_parquet(os.path.join(DIR, 'goodreads_cleaned_7000.parquet'))
-
+#df_books.to_parquet(os.path.join(DIR, 'goodreads_cleaned_10000.parquet'))
 
 
 
